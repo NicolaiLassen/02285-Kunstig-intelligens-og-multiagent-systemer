84201f7f7b492d17b33710dd9109603dd121cba2
Michael Guldborg <mail@michaelguldborg.dk>
2021-08-28 14:05:48 +0200

    Add class

--- project/dotnet_sol/MaMapF/Handlers/SearchHandler.cs
+++ project/dotnet_sol/MaMapF/Handlers/SearchHandler.cs
@@ -169,4 +169,41 @@ namespace MaMapF.Handlers
             delegation.InitialStates = agentInitialStates;
         }
     }
+    
+    
+    
+    public class SingleAgentProblem
+    {
+        public char AgentName { get; set; }
+        public SingleAgentState InitialState { get; set; }
+        public List<MapItem> Goals { get; set; } = new List<MapItem>();
+
+        public List<Position> WallModifications { get; set; } = new List<Position>();
+        public List<MapItem> BoxModifications { get; set; } = new List<MapItem>();
+
+        public void ResetInitialState()
+        {
+            foreach (var position in WallModifications)
+            {
+                InitialState.Walls.Remove($"{position.Row},{position.Column}");
+            }
+
+            foreach (var box in BoxModifications)
+            {
+                InitialState.Boxes.Add(box);
+            }
+
+            WallModifications = new List<Position>();
+            BoxModifications = new List<MapItem>();
+        }
+
+        public override string ToString()
+        {
+            var goalString = string.Join("\n", Goals.Select(g => g.ToString()));
+            return $"SingleAgentProblem {AgentName}\n" +
+                   $"{InitialState}" +
+                   $"GOALS\n{goalString}\n";
+        }
+    }
+    
 }
